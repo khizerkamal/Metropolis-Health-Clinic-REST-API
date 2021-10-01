9efa608628aec120fc0d66dc6ecb351fcee8f004
Muhammad Khizer Kamal <khizerkamal.03@gmail.com>
2021-10-01 17:44:40 +0500

    treatment-bug-fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -59,9 +59,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
     sendErrorDev(err, res)
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === 'PRODUCTION') {
     let error = { ...err }
 
     if (error.name === 'CastError') error = handleCastErrorDB(error)
--- controllers/treatmentController.js
+++ controllers/treatmentController.js
@@ -1,4 +1,5 @@
 const Treatment = require('../models/treatmentModel')
+const Appointment = require('../models/appointmentModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.treatment = catchAsync(async (req,res,next) => {
@@ -6,6 +7,11 @@ exports.treatment = catchAsync(async (req,res,next) => {
         appointmentId: req.body.appointmentId,
         treatment: req.body.treatment
     })
+  
+    const appointment = await Appointment.findById(treatment.appointmentId)
+    console.log(appointment)
+    appointment.status = 'done'
+    await appointment.save();
 
     res.status(201).json({
         status: 'success',
